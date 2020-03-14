9f61d754b4a4411f0af879cb91635f1eb3c45d0d
shoyou9527 <shoyou123@gmail.com>
2020-03-14 02:51:25 +0000

    TS刪除無用檔

--- resources/views_AFTER/dashboard/cancel.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.master')
-
-@section('app-content')
-
-
-@stop
--- resources/views_AFTER/partials/scrollup.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="m-scroll-top m-scroll-top--skin-top" data-toggle="m-scroll-top" data-scroll-offset="500" data-scroll-speed="300">
-		<i class="la la-arrow-up"></i>
-	</div>
