60e7f0191212edf9dceb50f953654336780a8354
shoyou9527 <shoyou123@gmail.com>
2020-06-01 02:50:14 +0000

    production payment settings

--- config/ecpay.php
+++ config/ecpay.php
@@ -11,28 +11,28 @@
 
 return [
     'payment' => [
-        'MerchantID' => "2000132",
-        'uid' => "53538851",  //統一編號
-        'HashKey' => "5294y06JbISpM5x9",
-        'HashIV' => "v77hoKGq4kWxNNIS",
-        //'ActionURL' => "https://payment.ecpay.com.tw/Cashier/AioCheckOut/V5",  //送出訂單的網址
-        'ActionURL' => "https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5",  //送出訂單的網址(測試用)
-        'ReturnURL' => "http://www.sugar-garden.org/dashboard/upgradepayEC",  //背景傳送付款結果的網址
-        'postChatpayReturnURL' => "https://www.sugar-garden.org/dashboard/postChatpayEC",  //背景傳送車馬費付款結果的網址
-        'PeriodReturnURL' => "http://www.sugar-garden.org/dashboard/upgradepayEC",  //背景傳送定期定額付款交易結果的網址
-        'ClientBackURL' => "http://www.sugar-garden.org/dashboard", //返回商店的網址
-        // 'OrderResultURL' => "http://www.sugar-garden.org/dashboard/upgradepay",  //付款結果的網址，若不設則會使用綠界的付款結果
+        'MerchantID' => "3137610",
+        'uid' => "24470001",  //統一編號
+        'HashKey' => "BOerb1FcOOjccN8o",
+        'HashIV' => "KOBKiDuvxIvjCSBz",
+        'ActionURL' => "https://payment.ecpay.com.tw/Cashier/AioCheckOut/V5",  //送出訂單的網址
+        // 'ActionURL' => "https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5",  //送出訂單的網址(測試用)
+        'ReturnURL' => "http://taiwan-sugar.net/dashboard/upgradepayEC",  //背景傳送付款結果的網址
+        'postChatpayReturnURL' => "https://taiwan-sugar.net/dashboard/postChatpayEC",  //背景傳送車馬費付款結果的網址
+        'PeriodReturnURL' => "http://taiwan-sugar.net/dashboard/upgradepayEC",  //背景傳送定期定額付款交易結果的網址
+        'ClientBackURL' => "http://taiwan-sugar.net/dashboard", //返回商店的網址
+        // 'OrderResultURL' => "http://taiwan-sugar.net/dashboard/upgradepay",  //付款結果的網址，若不設則會使用綠界的付款結果
     ],
     'payment_test' => [
         'MerchantID' => "2000132",
         'uid' => "53538851",  //統一編號
         'HashKey' => "5294y06JbISpM5x9",
         'HashIV' => "v77hoKGq4kWxNNIS",
-        'ActionURL' => "https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5",  //送出訂單的網址
-        'ReturnURL' => "https://ts.test-tw.icu/dashboard/upgradepayEC",  //背景傳送付款結果的網址
-        'postChatpayReturnURL' => "https://ts.test-tw.icu/dashboard/postChatpayEC",  //背景傳送車馬費付款結果的網址
-        'PeriodReturnURL' => "https://ts.test-tw.icu/dashboard/upgradepayEC",  //背景傳送定期定額付款交易結果的網址
-        'ClientBackURL' => "http://ts.test-tw.icu/dashboard", //返回商店的網址
-        // 'OrderResultURL' => "http://ts.test-tw.icu/dashboard/upgradepay",  //付款結果的網址，若不設則會使用綠界的付款結果
+        'ActionURL' => "https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5",  //送出訂單的網址(測試用)
+        'ReturnURL' => "https://taiwan-sugar.net/dashboard/upgradepayEC",  //背景傳送付款結果的網址
+        'postChatpayReturnURL' => "https://taiwan-sugar.net/dashboard/postChatpayEC",  //背景傳送車馬費付款結果的網址
+        'PeriodReturnURL' => "https://taiwan-sugar.net/dashboard/upgradepayEC",  //背景傳送定期定額付款交易結果的網址
+        'ClientBackURL' => "http://taiwan-sugar.net/dashboard", //返回商店的網址
+        // 'OrderResultURL' => "http://taiwan-sugar.net/dashboard/upgradepay",  //付款結果的網址，若不設則會使用綠界的付款結果
     ],
 ];
