7a5cb663e8190ba9ef111a053d12350207873de0
shoyou9527 <shoyou123@gmail.com>
2020-06-08 08:12:07 +0000

    set ecpay https

--- config/ecpay.php
+++ config/ecpay.php
@@ -17,10 +17,10 @@ return [
         'HashIV' => "KOBKiDuvxIvjCSBz",
         'ActionURL' => "https://payment.ecpay.com.tw/Cashier/AioCheckOut/V5",  //送出訂單的網址
         // 'ActionURL' => "https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5",  //送出訂單的網址(測試用)
-        'ReturnURL' => "http://taiwan-sugar.net/dashboard/upgradepayEC",  //背景傳送付款結果的網址
+        'ReturnURL' => "https://taiwan-sugar.net/dashboard/upgradepayEC",  //背景傳送付款結果的網址
         'postChatpayReturnURL' => "https://taiwan-sugar.net/dashboard/postChatpayEC",  //背景傳送車馬費付款結果的網址
-        'PeriodReturnURL' => "http://taiwan-sugar.net/dashboard/upgradepayEC",  //背景傳送定期定額付款交易結果的網址
-        'ClientBackURL' => "http://taiwan-sugar.net/dashboard", //返回商店的網址
+        'PeriodReturnURL' => "https://taiwan-sugar.net/dashboard/upgradepayEC",  //背景傳送定期定額付款交易結果的網址
+        'ClientBackURL' => "https://taiwan-sugar.net/dashboard", //返回商店的網址
         // 'OrderResultURL' => "http://taiwan-sugar.net/dashboard/upgradepay",  //付款結果的網址，若不設則會使用綠界的付款結果
     ],
     'payment_test' => [
